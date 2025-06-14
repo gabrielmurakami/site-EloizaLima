d621d2edd47be44fcb80f3fade1abf8276e3771c
Gabriel Murakami <egabrielmurakami@gmail.com>
2025-06-14 12:42:08 -0300

    Corrige base do Vite para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./" , // muito importante!
+  base: "/site-EloizaLima/", // muito importante!
   server: {
     host: "::",
     port: 8080,
