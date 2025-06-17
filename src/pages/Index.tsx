86ddc32d5b5266ac5ef2b059beafa787552f127a
Gabriel Murakami <egabrielmurakami@gmail.com>
2025-06-17 17:57:43 -0300

    Revert "update"
    This reverts commit 84c80526efe1bb172ef8228b2e1e75d3fa2c64ae.

---
+++ src/pages/Index.tsx
@@ -0,0 +1,26 @@
+
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import About from '@/components/About';
+import Services from '@/components/Services';
+import Appointment from '@/components/Appointment';
+import Contact from '@/components/Contact';
+import Footer from '@/components/Footer';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Appointment />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
